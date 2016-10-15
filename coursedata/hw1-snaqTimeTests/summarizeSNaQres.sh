8cd6676c5f50908911bf2f49da0155c94811548d
mortonwilliam <wmorton@wisc.edu>
2016-10-15 13:45:21 -0500

    updated summmary scripts excercise 3

--- coursedata/hw1-snaqTimeTests/summarizeSNaQres.sh
+++ coursedata/hw1-snaqTimeTests/summarizeSNaQres.sh
@@ -1,61 +1,51 @@
-# WORKS loop below works for hmax in log files
 
-#for filename in log/*.log
-#do
-  #echo "my filename is $filename"
-  #grep -E -o 'hmax = [0-9]+' $filename | head -n 1 | grep -E -o '\d+'
-
-#done
-
-#for filename in out/*.out
-#do
-  #echo "my filename is $filename"
-  #grep -E -o 'hmax = [0-9]+' $filename | head -n 1 | grep -E -o '\d+'
-
-#done
-
-# WORKS
-#for filename in out/*.out
-#do
-  #echo "my filename is $filename"
-  #grep -E -o 'Elapsed time. \d+\.\d+' $filename | grep -E -o '\d+\.\d+'
-
-#done
-
-#test loop below, for root name, doesnt work
-
-#for filename in log/*.log
-#do
-  #echo "my filename is $filename"
-  #grep -E -o 'log/*.log' $filename | grep -E -o 'log/*.log'
-
-#done
-
-#for filename in out/.*out
-#do
-  #echo 'out/timetest10_snaq.out' | grep -o -E "^[^.]+"
-
-#done
-
-echo 'Filename, Hmax, CPU Time, main seed'
-for filename in out/*.out
+echo basename, hmax, CPUtime, Nruns, Nfail, fabs, frel, xabs, xrel, Seed, under3460, under3450, under3440 >> wmortonhw1.csv
+for filename in log/*.log
 do
 
-  echo "my filename is $filename"
-  myfirstline=$(echo "${filename}" | # shows file names
-                grep -o -E "[^/]+$" | #gives filename from / to end
-                grep -o -E "^[^.]+") #gives filename from start to "."
-  mysecondline=$(grep -E -o 'Elapsed time. \d+\.\d+' $filename |
-                grep -E -o '\d+\.\d+') #gives all digits before and then after "."
-  mythirdline=$(grep -E -o 'hmax = [0-9]+' log/${myfirstline}.log |
-                head -n 1 | # hmax appears ~10 times in the file
-                grep -E -o '\d+') # to get rid of hmax at the begining
+  Basename=$(echo "${filename}" | grep -o -E "[^/]+$" | grep -o -E "^[^.]+")
+  #gives filenames
+  Hmax=$(grep -E -o 'hmax = [0-9]+' log/${Basename}.log | head -n 1 | grep -E -o '\d+')
+  #hmax values from log files
+  CPUTime=$(grep -E -o 'Elapsed time. \d+\.\d+' out/${Basename}.out | grep -E -o '\d+\.\d+')
+  #gives all digits for CPUTime before and then after "."
+  Nruns=$(sed -n 'runs = [0-9]+' $filename | tail -n 1  | grep -o -E '\d+')
+  #"max number of failed proposals"
+  #still can get this one to work?
+  Nfail=$(sed -n '5p' $filename | grep -o -E 'proposals\s=\s\d+' | grep -o -E "\d+")
+  #max number of failed proposals
+  Seed=$(grep "main seed" $filename  | grep -E -o "\d+")
+  #main seed/seed for the first runs
+  frel=$(sed -n '3p' $filename | cut -f4 -d" " | grep -E -o "\d+.+\d+")
+  #ftolRel for each analysis
+  fabs=$(sed -n '3p' $filename | cut -f5 -d" " | grep -E -o "\d+.+\d+")
+  #ftolAbs in the log file
+  xabs=$(sed -n '4p' $filename | cut -f1 -d "," | grep -E -o "\d+.+\d+")
+  #xtolABS for each analysis
+  xrel=$(sed -n '4p' $filename | cut -f2 -d "," | grep -E -o "\d+.+\d+")
+  #xtolRel for each analysis
 
-  #Seed=$(grep -E -o 'main seed' log/${myfirstline}.log | grep -E -o '\d+')
-  #Seed= $(grep -E -o 'seed' ${myfirstline} | head -n 1 | cut -f3 -d "")
+loglik=$(grep "loglik of best" $filename | cut -f10 -d " " | cut -f1 -d.)
+i1=5000
+i2=5000
+i3=5000
+for lik in $loglik
+do
+  if [ $lik -lt 3460 ]
+  then
+    echo "i is less than 5000"
+  fi
 
-  echo 'Filename, Hmax, CPU Time, main seed'
-  echo "$myfirstline, $mythirdline, $mysecondline, $Seed"  >> morton.hw1.csv
+  if [ $lik -lt 3450 ]
+  then
+    echo "i is less than 5000"
+  fi
+
+  if [ $lik -lt 3440 ]
+  then
+  echo "i is less than 5000"
+  fi
 done
-
-#echo "$myfirstnumber, $mysecondnumber, $mythirdnumber" >> csv
+#loops still dont work
+  echo "$Basename, $Hmax, $CPUTime, $Nruns, $Nfail, $fabs, $frel, $xabs, $xrel, $Seed, $under3460, $under3450, $under3440"  >> wmortonhw1.csv
+done
